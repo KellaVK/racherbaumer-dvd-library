f1746ea35b272a40215caea43dc6db52768686e3
Kella Kruebbe <kellakruebbe@Kellas-Air.lan>
2026-05-12 21:17:10 -0500

    Fresh Start

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // This ensures assets are linked correctly at racherbaumer.kellakruebbe.com
+  base: '/', 
+
   build: {
     outDir: 'dist',
+    // Optional: This ensures the build is clean every time
+    emptyOutDir: true,
   },
 })
